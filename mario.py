6fce7f56f255a48907f24c191727a85f67591d20
east_star00 <1219kdk@gmail.com>
2024-11-01 22:44:56 +0900

    Idle 상태 정의
    mairo.py 에서 Idle 상태 생성

--- mario.py
+++ mario.py
@@ -1,11 +1,29 @@
 from pico2d import load_image
 
+from state_machine import StateMachine
+
+class Idle:
+    @staticmethod
+    def enter(mario):
+        print('Mario Idle Enter')
+    @staticmethod
+    def exit(boy):
+        print('Boy Idle Exit')
+    @staticmethod
+    def do(boy):
+        boy.frame = (boy.frame + 1) % 8
+
+    @staticmethod
+    def draw(boy):
+        boy.image.clip_draw(boy.frame * 100, boy.action * 100, 100, 100, boy.x, boy.y)
+
 
 class Mario:
     def __init__(self):
             self.x, self.y = 100, 126
             self.frame = 0
             self.mario = load_image('small_mario_runningsheet.png')
+            self.state_machine = StateMachine(self)
     def update(self):
         self.frame = (self.frame+1) %4
         pass
