0d66526c706f8244617ecb2062218ca88284231b
east_star00 <1219kdk@gmail.com>
2024-11-01 18:21:07 +0900

    화면에 마리오 띄우기

--- main.py
+++ main.py
@@ -1,8 +1,17 @@
 import opening
 from pico2d import *
 
-from mario import Mario
-
+class Mario:
+    def __init__(self):
+            self.x, self.y = 0, 90
+            self.frame = 0
+            self.image = load_image('mario_image.png')
+    def update(self):
+        self.frame = (self.frame+1) %8
+        pass
+            # self.x += 5
+    def draw(self):
+            self.image.draw_now(400,30)
 
 def handle_events():
     global running
@@ -18,24 +27,26 @@ def handle_events():
 def reset_world():
     global running
     global mario
-    global world
+    #global world
 
     running = True
-    world = []
+    #world = []
 
     mario = Mario()
-    world.append(mario)
+    #world.append(mario)
 
 
 def update_world():
-    for o in world:
-        o.update()
+    # for o in world:
+    #     o.update()
+    mario.update()
     pass
 
 def render_world():
     clear_canvas()
-    for o in world:
-        o.draw()
+    # for o in world:
+    #     o.draw()
+    mario.draw()
     update_canvas()
 
 open_canvas()
--- mario.py
+++ mario.py
@@ -1,11 +1,17 @@
-import pico2d
-from pico2d import load_image
-
-
-class Mario:
-    def __init__(self):
-        self.x, self.y = 400, 90
-        self.frame = 0
-        self.action = 3
-        self.image = load_image('')
-        pass
+# import pico2d
+# from pico2d import load_image
+#
+#
+# class Mario:
+#     def __init__(self):
+#         self.x, self.y = 400, 90
+#         self.frame = 0
+#         self.action = 3
+#         self.image = load_image('resource/mario/mario_image.png')
+#
+#
+#     def draw(self):
+#         self.image.draw(400,30)
+#
+#     def update(self):
+#         pass
