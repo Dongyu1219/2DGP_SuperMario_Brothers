6aaeb80534fa897ea84fa9213ac8fd2218948e21
east_star00 <1219kdk@gmail.com>
2024-11-01 23:13:17 +0900

    state_machine.py 수정

--- mario.py
+++ mario.py
@@ -20,14 +20,16 @@ class Idle:
 class Sleep:
     @staticmethod
     def enter(mario):
-        mario.frame = 0
+        print('Mario Sleep Enter')
     @staticmethod
     def exit(mario):
+        print('Mario Sleep Exit')
         pass
     @staticmethod
     def do(mario):
         pass
     def draw(mario):
+        mario.sit_image.draw(mario.x, mario.y)
         pass
 
 
@@ -36,6 +38,7 @@ class Mario:
             self.x, self.y = 100, 126
             self.frame = 0
             self.image = load_image('small_mario_runningsheet.png')
+            self.sit_image = load_image('small_mario_sit_image.png')
             self.state_machine = StateMachine(self)
             self.state_machine.start(Idle)
     def update(self):
@@ -43,7 +46,9 @@ class Mario:
         #self.frame = (self.frame+1) %4
         pass
 
-    def handle_event(self):
+    def handle_event(self, event):
+        #튜플을 이용해서 이벤트 상태 나타내기
+        self.state_machine.handle_event(('INPUT', event))
         pass
 
     def draw(self):
--- state_machine.py
+++ state_machine.py
@@ -1,11 +1,34 @@
+from sdl2 import SDL_KEYDOWN, SDLK_SPACE
+
+def space_down(e):
+    return e[0] == 'INPUT' and e[1].type == SDL_KEYDOWN and e[1].key == SDLK_SPACE
+def time_out(e):
+    return e[0] == 'TIME_OUT'
+
+
 class StateMachine:
     def __init__(self, o):
         self.o = o
+        self.event_que = []
     def start(self, state):
         self.cur_state = state
-        self.cur_state.enter(self.o)
+        self.cur_state.enter(self.o, ('START', 0))
+    def add_event(self, e):
+        self.event_que.append(e)
+    def set_transition(self, transitions):
+        self.transtions = transitions
     def update(self):
         self.cur_state.do(self.o)
+        if self.event_que:
+            event = self.event_que.pop(0)
+            self.handle_event(event)
     def draw(self):
         self.cur_state.draw(self.o)
+    def handle_event(self,e ):
+        for event, next_state in self.transtions[self.cur_state].items():
+            if event(e):
+                self.cur_state.exit(self.o, e)
+                self.cur_state = next_state
+                self.cur_state.enter(self.o, e)
+                return
 
