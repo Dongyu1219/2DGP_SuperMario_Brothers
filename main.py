080fbc9de51d130132cf14c0dc09df6fdeca3ff8
east_star00 <1219kdk@gmail.com>
2024-11-01 19:13:11 +0900

    World 클래스 opening 클래스 만들기

--- main.py
+++ main.py
@@ -1,17 +1,31 @@
-import opening
 from pico2d import *
+
+class Opening:
+    def __init__(self):
+        self.opening = load_image('opening_image.png')
+        self.screen_size = 260
+        pass
+    def draw(self):
+        self.opening.draw_now(260, 260, self.screen_size, self.screen_size)
+    def update(self):
+        pass
 
 class Mario:
     def __init__(self):
             self.x, self.y = 0, 90
             self.frame = 0
-            self.image = load_image('small_mario_runningsheet.png')
+            self.mario = load_image('small_mario_runningsheet.png')
     def update(self):
         self.frame = (self.frame+1) %4
         pass
             # self.x += 5
     def draw(self):
-            self.image.clip_draw(self.frame*35, 0, 34, 26, 200, 200, 100, 100)
+            self.mario.clip_draw(self.frame*35, 0, 34, 26, 200, 132, 100, 100)
+
+class World_1:
+    def __init__(self):
+        self.x, self.y
+    pass
 
 def handle_events():
     global running
@@ -27,12 +41,14 @@ def handle_events():
 def reset_world():
     global running
     global mario
+    global opening
     #global world
 
     running = True
     #world = []
 
     mario = Mario()
+    opening = Opening()
     #world.append(mario)
 
 
@@ -47,6 +63,7 @@ def render_world():
     # for o in world:
     #     o.draw()
     mario.draw()
+    opening.draw()
     update_canvas()
 
 open_canvas()
